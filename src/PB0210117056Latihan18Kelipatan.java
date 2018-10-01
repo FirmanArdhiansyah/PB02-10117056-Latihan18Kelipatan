c73eb316a93dfba2a3e117e2c2af3d7d759ec8f8
FirmanArdhiansyah <firmanardhi14@gmail.com>
2018-10-01 11:22:54 +0700

    Latihan18

---
+++ src/PB0210117056Latihan18Kelipatan.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Nama     : Firman Ardhiansyah
+ *         Kelas    : IF-2 (PB02)
+ *         NIM      : 10117056
+ */
+public class PB0210117056Latihan18Kelipatan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       double bilangan;
+       bilangan =0;
+       while (bilangan <=35) {
+           bilangan = bilangan + 3.5;
+           System.out.println(bilangan);
+       }
+    }
+    
+}
